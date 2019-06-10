9e5333306da236d239e89100806300bac9a55cbd
Kanokwan Saeung <lawmena299@gmail.com>
2019-06-10 18:49:36 +0700

    WIP pen name

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -2,10 +2,19 @@
 <div class="collapse navbar-collapse navbar-toggleable-sm navbar-ex1-collapse">
     <ul class="nav navbar-nav side-nav list-group">
         <li class="list-group-item">
-            <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+            <a href="{!! url('cms/dashboard') !!}"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
         </li>
         <li class="list-group-item">
-            <a href="charts.html"><i class="fa fa-fw fa-bar-chart-o"></i> Charts</a>
+            <a href="javascript:;" data-toggle="collapse" data-target="#author"><i class="fa fa-fw fa-arrows-v"></i>
+                Authors <i class="fa fa-fw fa-caret-down"></i></a>
+            <ul id="author" class="list-group collapse">
+                <li class="list-group-item">
+                    <a href="javascript:;">Authors</a>
+                </li>
+                <li class="list-group-item">
+                    <a href="{!! url('cms/authors/pen-names/list') !!}">Pen Name</a>
+                </li>
+            </ul>
         </li>
         <li class="list-group-item">
             <a href="tables.html"><i class="fa fa-fw fa-table"></i> Tables</a>
@@ -20,7 +29,8 @@
             <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Bootstrap Grid</a>
         </li>
         <li class="list-group-item">
-            <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
+            <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i>
+                Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
             <ul id="demo" class="list-group collapse">
                 <li class="list-group-item">
                     <a href="javascript:;">Dropdown Item</a>
--- routes/cms.php
+++ routes/cms.php
@@ -2,3 +2,11 @@
 
 
 Route::get('dashboard', 'cms\DashboardController@dashboard');
+
+
+Route::group(['prefix' => '/authors'], function () {
+    Route::group(['prefix' => '/pen-names'], function () {
+        Route::get('/list', 'cms\PenNameController@index');
+
+    });
+});
